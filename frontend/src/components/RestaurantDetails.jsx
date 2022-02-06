ded794e5c496ea5364a38e378b02777b93c707b1
Putt <57494734+PuttTim@users.noreply.github.com>
2022-02-06 15:02:26 +0800

    Proof of Concept: Restaurant Detail Component

--- frontend/src/components/RestaurantDetails.jsx
+++ frontend/src/components/RestaurantDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import {
     Container,
     Paper,
@@ -6,14 +6,24 @@ import {
     Box,
     Card,
     Typography,
-    Grid
+    Grid,
+    Rating,
+    Stack,
+    Divider,
+    Dialog,
+    DialogTitle,
+    Link
 } from '@mui/material'
 
+import GoogleMapsModal from './GoogleMapsEmbed'
+
 const RestaurantDetails = () => {
+    const [openMaps, setOpenMaps] = useState(false)
+
     const restaurant = {
         id: 3,
-        name: 'Stewart-Rogers',
-        location: '8421 Jonathon Park',
+        name: 'Young, Nelson and Jackson',
+        location: '71362 Kelly Trace Davisburgh, MO 83041',
         pricing: '$$$',
         photo: [
             'https://images.unsplash.com/photo-1515668166700-4c11137632fc?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=540&w=720',
@@ -32,38 +42,180 @@ const RestaurantDetails = () => {
             'Sunday'
         ],
         opening: [
-            '10:30:00',
-            '10:30:00',
-            '10:30:00',
-            '10:30:00',
-            '10:30:00',
-            '10:30:00',
-            '10:30:00'
+            '10:30',
+            '10:30',
+            '10:30',
+            '10:30',
+            '10:30',
+            '10:30',
+            '10:30'
         ],
         closing: [
-            '13:00:00',
-            '13:00:00',
-            '13:00:00',
-            '13:00:00',
-            '13:00:00',
-            '13:00:00',
-            '13:00:00'
+            '13:00',
+            '13:00',
+            '13:00',
+            '13:00',
+            '13:00',
+            '13:00',
+            '13:00'
         ],
-        category: 'Halal',
+        category: 'Japanese • Korean • Western',
         avg_rating: 4.5,
         review_count: 2
     }
 
     return (
-        <Container maxWidth="md">
-            <Grid container>
-                <Grid item xs={12}>
-                    <Typography align="center" variant="h2">
-                        {restaurant.name}
-                    </Typography>
+        <>
+            <Dialog
+                onClose={() => {
+                    !setOpenMaps(false)
+                }}
+                open={openMaps}>
+                <DialogTitle>Google Maps</DialogTitle>
+                <GoogleMapsModal location={restaurant.location} />
+            </Dialog>
+            <Container maxWidth="md">
+                <Grid container rowSpacing={3}>
+                    <Grid item xs={12}>
+                        <Typography align="center" variant="h2">
+                            {restaurant.name}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider
+                            variant="middle"
+                            sx={{ borderBottomWidth: 5 }}
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Grid container rowSpacing={3}>
+                            <Grid item xs={12}>
+                                <Rating
+                                    value={restaurant.avg_rating}
+                                    precision={0.5}
+                                    size="large"
+                                    readOnly
+                                    sx={{}}
+                                />
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Divider
+                                    variant="middle"
+                                    sx={{ borderBottomWidth: 5 }}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography variant="h4">Cuisine</Typography>
+                                <Typography variant="h5" sx={{ mt: '10px' }}>
+                                    {restaurant.category}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Stack>
+                            {restaurant.days.map((day, index) => {
+                                return (
+                                    <div key={index}>
+                                        <Grid container>
+                                            <Grid item xs={6} md={1}>
+                                                <Typography align="left">
+                                                    {day}
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Typography align="right">{`${restaurant.opening[index]} - ${restaurant.closing[index]}`}</Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </div>
+                                )
+                            })}
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider
+                            variant="middle"
+                            sx={{ borderBottomWidth: 5 }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Stack spacing={3}>
+                            <Typography align="center" variant="h3">
+                                Located at
+                            </Typography>
+
+                            <Typography align="center" variant="h5">
+                                {restaurant.location}
+                            </Typography>
+                            <Button
+                                variant="contained"
+                                onClick={() => {
+                                    setOpenMaps(true)
+                                }}
+                                sx={{
+                                    color: '#ffffff'
+                                }}>
+                                View on Google Maps
+                            </Button>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider
+                            variant="middle"
+                            sx={{ borderBottomWidth: 5 }}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography align="center" variant="h3">
+                            Contacts
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h4">Website: </Typography>
+                        {restaurant.website.map((website, index) => {
+                            return (
+                                <Typography
+                                    align="left"
+                                    variant="h5"
+                                    key={index}>
+                                    <a href={`https://${website}`}>{website}</a>
+                                </Typography>
+                            )
+                        })}
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h4">Email: </Typography>
+                        {restaurant.email.map((email, index) => {
+                            return (
+                                <Typography
+                                    align="left"
+                                    variant="h5"
+                                    key={index}>
+                                    <a href={`mailto:${email}`}>{email}</a>
+                                </Typography>
+                            )
+                        })}
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography variant="h4">Mobile Number: </Typography>
+
+                        {restaurant.mobile_number.map((number, index) => {
+                            return (
+                                <Typography
+                                    align="left"
+                                    variant="h5"
+                                    key={index}>
+                                    {number.replace(
+                                        /\B(?=(?:\d{3})+(?!\d))/g,
+                                        '-'
+                                    )}
+                                </Typography>
+                            )
+                        })}
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </>
     )
 }
 
